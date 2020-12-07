b7cc42a1100fad1649fcc5cfa6645251b68a1490
TylerBoyd <tboyd1898@gmail.com>
2020-12-07 13:42:06 -0600

    added book model for mongoose

--- models/book.model.js
+++ models/book.model.js
@@ -1,1 +1,26 @@
 // mongoose book model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        genre: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
