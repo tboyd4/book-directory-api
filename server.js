fbf21dcfd33447d89743badfc9b19bb698dd461c
TylerBoyd <tboyd1898@gmail.com>
2020-12-07 13:33:35 -0600

    boilerplate server.js code and empty model and router files added

---
+++ server.js
@@ -0,0 +1,31 @@
+// server dependencies initiating
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// server variables
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware initiation
+app.use(cors());
+app.use(express.json());
+
+// mongoDB connection
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB Connection Established Successfully.');
+})
+
+// setting up router
+const bookRouter = require('./routes/books');
+app.use('/books', bookRouter);
+
+// initialize server
+app.listen(port, () => {
+    console.log(`Server Established. Listening on Port ${port}`);
+})
